fe5dc8548ef675c661e617e7d87296c6219e26cd
barte <barte@LAPTOP-EQ5SERF9>
2022-11-20 08:56:37 +0100

    observed

--- ScrapRefined/MauiProgram.cs
+++ ScrapRefined/MauiProgram.cs
@@ -27,7 +27,7 @@ public static class MauiProgram
 		builder.Services.AddScoped<ProductDetailViewModel>();
         builder.Services.AddTransient<AddProductViewModel>();
         builder.Services.AddTransient<CategoryViewModel>();
-
+        builder.Services.AddSingleton<ListObservedViewModel>();
         return builder.Build();
 	}
 }
